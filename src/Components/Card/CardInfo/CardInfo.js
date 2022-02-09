f72603b15cd35b0e5ad23b6529ff4a26ac7bab0e
DanhVanPham <danhskipper18@gmail.com>
2022-02-09 10:26:53 +0700

    [DanhPV] fix ui

--- src/Components/Card/CardInfo/CardInfo.js
+++ src/Components/Card/CardInfo/CardInfo.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Calendar, CheckSquare, List, Tag, Trash, Type } from "react-feather";
+import {
+  Calendar,
+  CheckSquare,
+  List,
+  Tag,
+  Trash,
+  Type,
+  X,
+} from "react-feather";
 import Chip from "../../Chip/Chip";
 import Editable from "../../Editable/Editable";
 import { v4 as uuidv4 } from "uuid";
@@ -90,130 +98,135 @@ function CardInfo(props) {
     <div>
       <Modal onClose={() => props.onClose()}>
         <div className="cardinfo">
-          <div className="cardinfo_box">
-            <div className="cardinfo_box_title">
-              <Type />
-              <span>Title</span>
-            </div>
-            <div className="cardinfo_box_body">
-              <Editable
-                text={values.title}
-                autoCloseAfterSubmit
-                default={values.title}
-                buttonText="Set Title"
-                placeholder="Enter Title"
-                onSubmit={(value) => handleChange("title", value)}
-              />
-            </div>
+          <div className="cardinfo_top">
+            <X onClick={() => props.onClose()} />
           </div>
-          <div className="cardinfo_box">
-            <div className="cardinfo_box_title">
-              <List />
-              <span>Description</span>
+          <div className="cardinfo_content">
+            <div className="cardinfo_box">
+              <div className="cardinfo_box_title">
+                <Type />
+                <span>Title</span>
+              </div>
+              <div className="cardinfo_box_body">
+                <Editable
+                  text={values.title}
+                  autoCloseAfterSubmit
+                  default={values.title}
+                  buttonText="Set Title"
+                  placeholder="Enter Title"
+                  onSubmit={(value) => handleChange("title", value)}
+                />
+              </div>
             </div>
-            <div className="cardinfo_box_body">
-              <Editable
-                text={values.desc || "Enter description"}
-                default={values.desc}
-                autoCloseAfterSubmit
-                placeholder="Enter Description"
-                buttonText="Set Description"
-                onSubmit={(value) => handleChange("desc", value)}
-              />
+            <div className="cardinfo_box">
+              <div className="cardinfo_box_title">
+                <List />
+                <span>Description</span>
+              </div>
+              <div className="cardinfo_box_body">
+                <Editable
+                  text={values.desc || "Enter description"}
+                  default={values.desc}
+                  autoCloseAfterSubmit
+                  placeholder="Enter Description"
+                  buttonText="Set Description"
+                  onSubmit={(value) => handleChange("desc", value)}
+                />
+              </div>
             </div>
-          </div>
-          <div className="cardinfo_box">
-            <div className="cardinfo_box_title">
-              <Calendar />
-              <span>Date</span>
-            </div>
-            <div className="cardinfo_box_body">
-              <input
-                className="cardinfo_box_body_input"
-                type="date"
-                defaultValue={
-                  values.date
-                    ? new Date(values.date).toISOString().substr(0, 10)
-                    : ""
-                }
-                onChange={(e) => handleChange("date", e.target.value)}
-              />
-            </div>
-          </div>
-
-          <div className="cardinfo_box">
-            <div className="cardinfo_box_title">
-              <Tag />
-              <span>Labels</span>
-            </div>
-            <div className="cardinfo_box_labels">
-              {values.labels &&
-                values.labels.length > 0 &&
-                values.labels.map((item) => (
-                  <Chip
-                    close
-                    text={item.text}
-                    key={item.id}
-                    color={item.color}
-                    onClose={() => handleDeleteLabel(item.id)}
-                  />
-                ))}
-            </div>
-            <div className="cardinfo_box_colors">
-              {colors.map((color, index) => (
-                <li
-                  key={index}
-                  className={color === activeColor ? "active" : ""}
-                  style={{
-                    backgroundColor: color,
-                  }}
-                  onClick={() => setActiveColor(color)}
-                ></li>
-              ))}
-            </div>
-            <div className="cardinfo_box_body">
-              <Editable
-                text="Enter Label"
-                placeholder="Enter label"
-                buttonText="Add Label"
-                onSubmit={(value) => handleChange("labels", value)}
-              />
-            </div>
-          </div>
-          <div className="cardinfo_box">
-            <div className="cardinfo_box_title">
-              <CheckSquare />
-              <span>Tasks</span>
-            </div>
-            <div className="cardinfo_box_progress-bar">
-              <div
-                className="cardinfo_box_progress"
-                style={{ width: calculateProgress() + "%" }}
-              ></div>
+            <div className="cardinfo_box">
+              <div className="cardinfo_box_title">
+                <Calendar />
+                <span>Date</span>
+              </div>
+              <div className="cardinfo_box_body">
+                <input
+                  className="cardinfo_box_body_input"
+                  type="date"
+                  defaultValue={
+                    values.date
+                      ? new Date(values.date).toISOString().substr(0, 10)
+                      : ""
+                  }
+                  onChange={(e) => handleChange("date", e.target.value)}
+                />
+              </div>
             </div>
 
-            <div className="cardinfo_box_list">
-              {values.tasks &&
-                values.tasks.length > 0 &&
-                values.tasks.map((item) => (
-                  <div className="cardinfo_task" key={item.id}>
-                    <input
-                      type="checkbox"
-                      checked={item.completed}
-                      onChange={() => props.changeStatusTask(item.id)}
+            <div className="cardinfo_box">
+              <div className="cardinfo_box_title">
+                <Tag />
+                <span>Labels</span>
+              </div>
+              <div className="cardinfo_box_labels">
+                {values.labels &&
+                  values.labels.length > 0 &&
+                  values.labels.map((item) => (
+                    <Chip
+                      close
+                      text={item.text}
+                      key={item.id}
+                      color={item.color}
+                      onClose={() => handleDeleteLabel(item.id)}
                     />
-                    <p>{item.text}</p>
-                    <Trash onClick={() => handleDeleteTask(item.id)} />
-                  </div>
+                  ))}
+              </div>
+              <div className="cardinfo_box_colors">
+                {colors.map((color, index) => (
+                  <li
+                    key={index}
+                    className={color === activeColor ? "active" : ""}
+                    style={{
+                      backgroundColor: color,
+                    }}
+                    onClick={() => setActiveColor(color)}
+                  ></li>
                 ))}
+              </div>
+              <div className="cardinfo_box_body">
+                <Editable
+                  text="Enter Label"
+                  placeholder="Enter label"
+                  buttonText="Add Label"
+                  onSubmit={(value) => handleChange("labels", value)}
+                />
+              </div>
             </div>
-            <div className="cardinfo_box_body">
-              <Editable
-                text="Add new task"
-                placeholder="Enter task"
-                buttonText="Add Task"
-                onSubmit={(value) => handleChange("tasks", value)}
-              />
+            <div className="cardinfo_box">
+              <div className="cardinfo_box_title">
+                <CheckSquare />
+                <span>Tasks</span>
+              </div>
+              <div className="cardinfo_box_progress-bar">
+                <div
+                  className="cardinfo_box_progress"
+                  style={{ width: calculateProgress() + "%" }}
+                ></div>
+              </div>
+
+              <div className="cardinfo_box_list">
+                {values.tasks &&
+                  values.tasks.length > 0 &&
+                  values.tasks.map((item) => (
+                    <div className="cardinfo_task" key={item.id}>
+                      <input
+                        type="checkbox"
+                        checked={item.completed}
+                        onChange={() => props.changeStatusTask(item.id)}
+                      />
+                      <p>{item.text}</p>
+                      <Trash onClick={() => handleDeleteTask(item.id)} />
+                    </div>
+                  ))}
+              </div>
+              <div className="cardinfo_box_body">
+                <Editable
+                  text="Add new task"
+                  placeholder="Enter task"
+                  buttonText="Add Task"
+                  onSubmit={(value) => handleChange("tasks", value)}
+                />
+              </div>
             </div>
           </div>
         </div>
